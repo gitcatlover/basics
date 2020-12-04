fad9d14908aeaea48f83c409ef6ac8d6d34d91f9
gitcat <547284251@qq.com>
2020-12-04 18:18:19 +0800

    test

--- 程序建议/Program.cs
+++ 程序建议/Program.cs
@@ -6,8 +6,12 @@ namespace 程序建议
     {
         static void Main(string[] args)
         {
+            //Readonly
             Person person = new Person(new Student() { Name = "测试" });
             //person.readonlyValue = new Student();//无法对制度字段赋值（构造函数或变量初始值可以赋值）
+            //引用本身不可变，引用所指的实例的值是可以改变的
+            person.readonlyValue.Name = "测试2";
+            Console.WriteLine(Week.Monday);
             Console.WriteLine("Hello World!");
         }
     }
@@ -26,4 +30,9 @@ public class Person
 public class Student
 {
     public string Name { get; set; }
+}
+
+public enum Week
+{
+    Monday=0
 }
