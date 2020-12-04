db4535eac89f0b001ccbb20e5440b4dbf04dd02b
gitcat <547284251@qq.com>
2020-12-04 16:23:48 +0800

    合并仓库

---
+++ 程序建议/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace 程序建议
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person(new Student() { Name = "测试" });
+            //person.readonlyValue = new Student();//无法对制度字段赋值（构造函数或变量初始值可以赋值）
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+public class Person
+{
+   public readonly Student readonlyValue;
+    public Person(Student student)
+    {
+        readonlyValue = student;
+    }
+
+    public int ID { get; set; }
+}
+public class Student
+{
+    public string Name { get; set; }
+}
